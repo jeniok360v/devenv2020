816b9b0fdf9478e96fb840969313b61075616d04
jeniok360v <gerhugwsif@gmail.com>
2020-10-20 14:50:59 +0200

    zad6.sh

--- zad6.sh
+++ zad6.sh
@@ -1,4 +1,5 @@
 #!bin/bash 
+NEWLINE=$'\n'
 (
 	cd $1/ ;
 	wordsStr=$( cat  $(find . -maxdepth 64 -type f)  |tr '[A-Z]' '[a-z]' |tr ' \t' '\n\n'|sed -e "/^$/d" |sort |uniq | tr '\n' ' ' )
@@ -8,19 +9,20 @@
 	files=($filesStr)
 
 	declare -a wordlist
-	for (( i = 0; i < ${#words[@]}; i++ )); 
-	do
-		wordlist[i]=${words[i]};
-	done
 
 	for((j=0;j<${#files[@]};j++))
 	do
 		for((k=0;k<${#words[@]};k++))
 		do
-			tmp=$(awk "BEGIN{IGNORECASE = 1};/${words[k]}/{print NR}" ${files[j]} | tr '\n' ' ')
-			if [ ! -z "$tmp" ]
+			tmp=$(grep -o -i ${words[k]} ${files[j]} | wc -l)
+			tmp2=$(grep -n -i ${words[k]} ${files[j]})
+			if [ $tmp -ge 2 ]
 			then
-				wordlist[k]="${wordlist[k]} ${files[j]}: $tmp"
+				if [  wordlist[k]=="" ]
+				then
+					wordlist[k]=${words[k]};
+				fi
+				wordlist[k]="${wordlist[k]} ${files[j]}: ${NEWLINE}$tmp2${NEWLINE}"
 			fi
 		done 
 	done
